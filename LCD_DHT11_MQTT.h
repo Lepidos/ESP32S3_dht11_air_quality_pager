1440c634b2091771d8b1d5568886e718ea4c2a1b
lepidos <lepidos@aorus.WORKGROUP>
2024-09-22 15:48:04 +0100

    update config header

--- LCD_DHT11_MQTT.h
+++ LCD_DHT11_MQTT.h
@@ -10,8 +10,8 @@
   #define SNAME           (String[]){"CO", "NH3"}         	// SENSOR NAME/DESCRIPTION
 
   #define MQTTADDR        (const char *[]){"192.168.1.73"}    // MQTT broker IP address
-  #define MQTTUSERNAME    (char*)"casa"          // MQTT authentification username
-  #define MQTTPASSWORD    (char*)"myhome"       // MQTT authentification passord
+  #define MQTTUSERNAME    (char*)"mqtt_username"          // MQTT authentification username
+  #define MQTTPASSWORD    (char*)"mqtt_password"       // MQTT authentification passord
   #define MQTT_PAYLOAD_S  512
   #define TOPIC_PREFIX    (String) "/"
   #define MQTT_DISCON     (const char *) "/disconnect/"
